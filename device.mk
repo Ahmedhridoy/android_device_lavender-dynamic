bc02795172a3cc5afff264f913d696bca74d85df
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2023-09-20 06:48:11 +0600

    lavender : Change powerhint json 636 to 660

--- device.mk
+++ device.mk
@@ -372,7 +372,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/powerhint_636.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_636.json \
+    $(DEVICE_PATH)/configs/powerhint_660.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint_660.json \
     $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 PRODUCT_SOONG_NAMESPACES += \
