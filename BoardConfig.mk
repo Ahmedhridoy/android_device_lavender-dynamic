cadbad0e3cf205348756fca3bf8bfdfb1b9a6f4b
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2023-09-20 13:21:54 +0600

    lavender: Override the hardware variant of qcom as sdm660

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,7 @@ BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 # Platform
 BOARD_VENDOR := xiaomi
 TARGET_BOARD_PLATFORM := sdm660
+OVERRIDE_QCOM_HARDWARE_VARIANT := sdm660
 #TARGET_ENFORCES_QSSI := true
 
 # Creates metadata partition mount point under root for
