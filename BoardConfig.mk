5081580a4c0ae436d8d833fdf35d667f6bc843e8
ahmedhridoy371 <ahmedhridoy371@gmail.com>
2023-09-20 19:12:35 +0600

    lavender : set TARGET_USES_UM_4_19

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,7 @@ PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := sdm660
 OVERRIDE_QCOM_HARDWARE_VARIANT := sdm660
 QCOM_HARDWARE_VARIANT := sdm660
+TARGET_USES_UM_4_19 := true
 
 # Creates metadata partition mount point under root for
 # the devices with metadata parition
